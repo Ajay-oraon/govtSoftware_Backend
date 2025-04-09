ca357c8ce16bdb45f041faf3f62dda862f378707
Ajay-oraon <jaioraon17@gmail.com>
2025-04-09 15:13:23 +0530

    fix cors error

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const sequelize = require("./src/config/database");
 const authRoutes = require("./src/routes/authRoutes");
 const workRoutes = require("./src/routes/workRoutes");
@@ -7,6 +8,16 @@ const userRoutes = require("./src/routes/userRoutes");
 require("dotenv").config();
 const app = express();
 app.use(express.json());
+app.use(cors());
+
+// For production (more secure)
+app.use(
+  cors({
+    origin: ["http://localhost:53914", "https://your-flutter-web-domain.com"], // replace with actual Flutter web domain
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/work", workRoutes);
