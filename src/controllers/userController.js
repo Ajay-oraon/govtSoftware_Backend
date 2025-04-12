48b2222745c673a7146e43b627ff34c89516d468
Ajay-oraon <jaioraon17@gmail.com>
2025-04-12 15:31:07 +0530

    add admin delete func

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -14,7 +14,7 @@ class UserController {
       const newUser = await this.userModel.create(userData);
       // Remove password from response
       const { password, ...userWithoutPassword } = newUser.toJSON();
-      res.status(201).json({message:"User Created",userWithoutPassword});
+      res.status(201).json({ message: "User Created", userWithoutPassword });
     } catch (error) {
       res.status(500).json({ message: "Error creating user", error });
     }
@@ -41,7 +41,7 @@ class UserController {
       const users = await this.userModel.findAll({
         attributes: { exclude: ["password"] },
       });
-      res.status(200).json({users:users})
+      res.status(200).json({ users: users });
     } catch (error) {
       res.status(500).json({ message: "Error retrieving users", error });
     }
@@ -59,6 +59,22 @@ class UserController {
       res.json(user);
     } catch (error) {
       res.status(500).json({ message: "Error updating user", error });
+    }
+  }
+
+  async deleteUser(req, res) {
+    try {
+      const userId = req.params.id;
+      const adminId = req.user.id; // from authenticateToken
+
+      const user = await this.userModel.findByPk(userId);
+
+      if (!user) return res.status(404).json({ message: "User not found" });
+
+      await user.destroy();
+      res.status(200).json({ message: "User deleted successfully" });
+    } catch (error) {
+      res.status(500).json({ message: "Error deleting user", error });
     }
   }
 }
